7944dd8bc1597ec9d75d8e1006440b8d701c57ef
Mikhail Shilov <mih.shilov@gmail.com>
2021-10-14 15:39:10 +0300

    Add example objects with fihgters. Create template of addPlayer()

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('Fight!');
+console.log('Start game!');
+
+const scorpion = {
+    name: 'Scorpion',
+    hp: 100,
+    img: '',
+    weapon: ['snake'],
+    attack: function () {
+        console.log(`${this.name} fight!`)
+    }
+}
+
+const subzero = {
+    name: 'Subzero',
+    hp: 100,
+    img: '',
+    weapon: ['frost'],
+    attack: function () {
+        console.log(`${this.name} fight!`)
+    }
+}
+
+function createPlayer(playerPosition, fighter) {
+    console.log(playerPosition);
+    console.log(fighter);
+
+}
+
+createPlayer('fff', 'ttt')
+console.log(scorpion.attack())
+console.log(subzero.attack())
