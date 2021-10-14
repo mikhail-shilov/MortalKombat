5123903af9892cfacc460a075f5e82235d61c6d0
Mikhail Shilov <mih.shilov@gmail.com>
2021-10-14 17:36:29 +0300

    Now we can add characters to arena

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-console.log('Start game!');
-
 const scorpion = {
     name: 'Scorpion',
-    hp: 100,
-    img: '',
+    hp: 40,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['snake'],
     attack: function () {
         console.log(`${this.name} fight!`)
@@ -13,19 +11,48 @@ const scorpion = {
 const subzero = {
     name: 'Subzero',
     hp: 100,
-    img: '',
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['frost'],
     attack: function () {
         console.log(`${this.name} fight!`)
     }
 }
 
-function createPlayer(playerPosition, fighter) {
-    console.log(playerPosition);
-    console.log(fighter);
+function createPlayer(position, character) {
+    const fighter = {...character}
 
+    $arena = document.querySelector('.arenas');
+
+    const $player = document.createElement('div');
+    $player.classList.add(`player${position}`);
+
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add(`progressbar`);
+
+    const $life = document.createElement('div');
+    $life.classList.add(`life`);
+    $life.style.width = `${fighter.hp}%`;
+
+    const $name = document.createElement('div');
+    $name.classList.add(`name`);
+    $name.innerHTML = fighter.name;
+
+    const $character = document.createElement('div');
+    $character.classList.add(`character`);
+
+    const $image = document.createElement('img');
+    $image.src = fighter.img;
+
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+
+    $character.appendChild($image);
+
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+
+    $arena.appendChild($player);
 }
 
-createPlayer('fff', 'ttt')
-console.log(scorpion.attack())
-console.log(subzero.attack())
+createPlayer(1, subzero)
+createPlayer(2, scorpion)
