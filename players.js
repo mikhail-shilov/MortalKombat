12e71f64dfa9b97d32a788082c816f4d8a6a133d
Mikhail Shilov <mih.shilov@gmail.com>
2021-10-29 21:05:11 +0300

    Some little fixing around

--- players.js
+++ players.js
@@ -1,21 +1,17 @@
-const dummyPlayer = {
+export class Player {
+    constructor(position, { name, img }) {
+        this.position = position;
+        this.name = name;
+        this.img = img;
+        this.hp = 100;
+    }
     changeHP(hp) {
         this.hp = this.hp < hp ? 0 : this.hp - hp;
-    },
+    }
     elHP() {
         return document.querySelector(`.player${this.position} .life`);
-    },
+    }
     renderHP(el) {
         el.style.width = `${this.hp}%`;
     }
-};
-
-export function initPlayer(character, position) {
-    const { name, img } = character;
-    let player = Object.create(dummyPlayer);
-    player.name = name;
-    player.img = img;
-    player.hp = 100;
-    player.position = position;
-    return player;
 }
--- tools.js
+++ tools.js
@@ -1,1 +1,17 @@
 export const getRandom = (range) => Math.ceil(Math.random() * range);
+
+
+export const getTime = () => {
+    const date = new Date();
+    const time = {
+        hh: date.getHours(),
+        mm: date.getMinutes(),
+        ss: date.getSeconds()
+    };
+
+    for (let item in time) {
+        time[item] = time[item].toString().length > 1 ? `${time[item]}` : `0${time[item]}`;
+    }
+    
+    return `${time.hh}:${time.mm}:${time.ss}`;
+};
