e3eca109f73e1a1b3b5167664fe7ba457416adbd
Mikhail Shilov <mih.shilov@gmail.com>
2021-10-29 20:47:45 +0300

    Update class Log

--- game.js
+++ game.js
@@ -3,13 +3,14 @@ import { getRandomPlayer } from "./characters.js";
 import { createPlayer, createReloadButton, createOutcomeMessage } from "./dom.js";
 import { readControl, clearControl, disableControl } from "./control.js";
 import { doRandomAttack } from "./ai.js";
-import { createLogMessage } from './log.js';
+import { Log } from './log.js';
 
 export class Game {
     constructor() {
         this.$arena = document.querySelector('.arenas');
         this.$chat = document.querySelector('.chat');
         this.$control = document.querySelector('.control');
+        this.log = new Log(this.$chat);
         this.player1 = null;
         this.player2 = null;
     }
@@ -25,7 +26,8 @@ export class Game {
         this.player2 = new Player(2, getRandomPlayer());
         this.$arena.appendChild(createPlayer(this.player2));
 
-        createLogMessage(this.$chat, 'start', this.player1.name, this.player2.name);
+        //        createLogMessage(this.$chat, 'start', this.player1.name, this.player2.name);
+        this.log.start(this.player1.name, this.player2.name);
     }
     doKick() {
         const player1Attack = readControl(this.$control);
@@ -34,17 +36,18 @@ export class Game {
         if (player1Attack.hit !== player2Attack.block) {
             this.player2.changeHP(player1Attack.value);
             this.player2.renderHP(this.player2.elHP());
-            createLogMessage(this.$chat, 'hit', this.player1.name, this.player2.name, player1Attack.value, this.player2.hp);
+            this.log.hit(this.player1.name, this.player2.name, player1Attack.value, this.player2.hp);
         } else {
-            createLogMessage(this.$chat, 'defence', this.player1.name, this.player2.name);
+            this.log.block(this.player1.name, this.player2.name);
         }
 
         if (player2Attack.hit !== player1Attack.block) {
             this.player1.changeHP(player2Attack.value);
             this.player1.renderHP(this.player1.elHP());
-            createLogMessage(this.$chat, 'hit', this.player2.name, this.player1.name, player2Attack.value, this.player1.hp);
+            this.log.hit(this.player2.name, this.player1.name, player2Attack.value, this.player1.hp);
+
         } else {
-            createLogMessage(this.$chat, 'defence', this.player2.name, this.player1.name);
+            this.log.block(this.player2.name, this.player1.name);
         }
 
         clearControl(this.$control);
@@ -55,11 +58,11 @@ export class Game {
             } else {
                 if (this.player1.hp <= 0) {
                     this.$arena.appendChild(createOutcomeMessage(`${this.player2.name} wins!`));
-                    createLogMessage(this.$chat, 'end', this.player2.name, this.player1.name);
+                    this.log.end(this.player2.name, this.player1.name);
                 }
                 if (this.player2.hp <= 0) {
                     this.$arena.appendChild(createOutcomeMessage(`${this.player1.name} wins!`));
-                    createLogMessage(this.$chat, 'end', this.player1.name, this.player2.name);
+                    this.log.end(this.player1.name, this.player2.name);
                 }
             }
             this.$arena.appendChild(createReloadButton());
